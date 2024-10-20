df667ff1851b322d0610891c58e2b8baa4f46437
neonew <neonew@gmail.com>
2024-10-20 22:10:03 +0200

    Periodically write to the serial console

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,7 @@
-// Original file taken from:
-// https://github.com/esp-rs/esp8266-hal/blob/c1d0b06b611f571237dda0bfebd688b96a5dc62a/examples/blinky.rs
-
 #![no_std]
 #![no_main]
 
+use core::fmt::Write;
 use esp8266_hal::prelude::*;
 use esp8266_hal::target::Peripherals;
 use panic_halt as _;
@@ -14,11 +12,23 @@ fn main() -> ! {
     let pins = dp.GPIO.split();
     let mut led = pins.gpio2.into_push_pull_output();
     let (mut timer1, _) = dp.TIMER.timers();
+    let mut serial = dp
+        .UART0
+        .serial(pins.gpio1.into_uart(), pins.gpio3.into_uart());
+
+    write!(serial, "\r\nHi there! {}:\r\n", 3).unwrap();
 
     led.set_high().unwrap();
 
     loop {
-        timer1.delay_ms(500);
+        write!(serial, "\r\nNext loop\r\n").unwrap();
+
+        timer1.delay_ms(1000);
         led.toggle().unwrap();
+        timer1.delay_ms(1000);
+        led.toggle().unwrap();
+        timer1.delay_ms(1000);
+        led.toggle().unwrap();
+        timer1.delay_ms(50000);
     }
 }
